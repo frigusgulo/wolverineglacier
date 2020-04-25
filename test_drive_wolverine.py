4fd317635c93a2f6ba67a668167515f831d42ea2
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2020-04-24 18:45:00 -0600

    .

--- test_drive_wolverine.py
+++ test_drive_wolverine.py
@@ -34,10 +34,11 @@ for path in pathlist:
     image = glimpse.Image(path=path,cam=path.replace(".JPG",".json"))
     print(image.datetime)
     images.append(image)
-
-observer = glimpse.Observer(list(np.array(images)))
+observer = []
+observer.append(glimpse.Observer(list(np.array(images))))
 print("==========================")
-print("Position {}".format(observer.xyz))
+print("Position {}".format(observer[0].xyz))
+#----------------------------
 # Prepare DEM
 ''' 
 boxes = [obs.images[0].cam.viewbox(MAX_DEPTH)
@@ -55,21 +56,30 @@ dem.fill_crevasses(mask=~np.isnan(dem.Z), fill=True)
 
 # ---- Prepare viewshed ----
 
-dem.fill_circle(observer.xyz, radius=100)
+dem.fill_circle(observer[0].xyz, radius=100)
 viewshed = dem.copy()
 viewshed.Z = np.ones(dem.shape, dtype=bool)
-viewshed.Z &= dem.viewshed(observer.xyz)
+#viewshed.Z &= dem.viewshed(observer[0].xyz)
 
 # ---- Run Tracker ----
 
-xy= np.array((394368,6696220))
+xy0= np.array((394368,6696220))
+xy = xy0 + np.vstack([xy for xy in
+    itertools.product(range(-10, 10, 1), range(-10, 10, 1))])
 
 
-time_unit = datetime.timedelta(days=0.5)
-motion_model = glimpse.CartesianMotionModel(xy, time_unit=time_unit, dem=dem, dem_sigma=3, n=5000, xy_sigma=(2, 2),vxyz_sigma=(5, 5, 0.2), axyz_sigma=(2, 2, 0.2))
+motion_model = []
+time_unit = datetime.timedelta(days=1)
+for i in range(xy.shape[0]):
+    for j in range(xy.shape[1]):
+        motion_model.append(glimpse.CartesianMotionModel(xy, time_unit=time_unit, dem=dem, dem_sigma=3, n=5000, xy_sigma=(2, 2),vxyz_sigma=(5, 5, 0.4), axyz_sigma=(2, 2, 0.2)))
 
-tracker = glimpse.Tracker(observers=observers, viewshed=viewshed)
+
+tracker = glimpse.Tracker(observers=observer, viewshed=viewshed)
 tracks = tracker.track(motion_models=motion_model, tile_size=(15, 15),parallel=32)
+
+print("====================================")
+print(tracks.xyz)
 
 # ---- Plot tracks ----
 tracks.plot_xy(start=dict(color='green'), mean=dict(color='red'), sigma=dict(alpha=0.25))
