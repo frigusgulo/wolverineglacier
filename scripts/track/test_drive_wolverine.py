29ef1fd239dd529c9f2f45f3db640c1af212771f
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2020-08-19 09:48:51 -0600

    .

--- scripts/track/test_drive_wolverine.py
+++ scripts/track/test_drive_wolverine.py
@@ -9,38 +9,28 @@ import itertools
 os.environ['OMP_NUM_THREADS'] = '1'
 #==============================================
 
-DATA_DIR = '/home/dunbar/Research/wolverine/data'
+DATA_DIR = '/home/dunbar/Research/wolverine/data/'
 DEM_DIR = os.path.join(DATA_DIR, 'dem')
 IMG_DIR = 'images'
 CAM_DIR = 'images_json'
 MAX_DEPTH = 30e3
-STATIONS = ('cam_tounge','cam_weather','cam_cliff')
+STATIONS = ('cam_cliff')
 
 # ---- Prepare Observers ----
 
-start = datetime.datetime(2017, 12, 30, 00)
-end = datetime.datetime(2019, 8, 30, 00)
-
 observers = []
-for station in STATIONS:
-    station_dir = os.path.join(DATA_DIR, station)
-    path = os.path.join(station_dir,CAM_DIR,'*.json')
-    cam_paths = glob.glob(path,recursive=True)
-    cam_paths.sort()
-    basenames = [glimpse.helpers.strip_path(path) for path in cam_paths]
-    images = [glimpse.Image(
-        path=os.path.join(station_dir, IMG_DIR, basename + '.JPG'),
-        cam=os.path.join(station_dir, CAM_DIR, basename + '.json'))
-        for basename in basenames]
-    datetimes = np.array([img.datetime for img in images])
-    inrange = np.logical_and(datetimes > start, datetimes < end)
-    observers.append(glimpse.Observer(list(np.array(images)[inrange])))
+
+campaths=  glob.glob(os.path.join("/home/dunbar/Research/wolverine/data/cam_cliff/images","*.JSON"))
+images = [glimpse.Image(path=campath.replace(".JSON",".JPG"),cam=campath) for campath in campaths]
+images.sort(key= lambda img: img.datetime)
+observers.append(glimpse.Observer(list(np.array(images))))
+#--------------------------
+
+
 #----------------------------
 # Prepare DEM 
-paths = glob.glob(os.path.join(DEM_DIR, '*.tiff'))
-paths.sort()
-path = paths[0]
-dem = glimpse.Raster.read(path)
+path = "/home/dunbar/Research/wolverine/data/dem/wolverine_skyline32606.tif"
+dem = glimpse.Raster.read(path,d=5)
 print("DEM PATH: {}".format(path))
 dem.crop(zlim=(0, np.inf))
 dem.fill_crevasses(mask=~np.isnan(dem.Z), fill=True)
@@ -56,12 +46,13 @@ viewshed.Z = np.ones(dem.shape, dtype=bool)
 for obs in observers:
     viewshed.Z &= dem.viewshed(obs.xyz)
 
+print("\n *****Viewshed Done**** \n")
 # ---- Run Tracker ----
 xy = []
-xy0 = np.array((-3.18068e06,787898))
+xy0 = np.array((394368,6696220))
 xy.append(xy0)
 #xy = xy0 + np.vstack([xy for xy in
-#    itertools.product(range(-500, 500, 50), range(-500, 500, 50))])
+  # itertools.product(range(-500, 500, 50), range(-500, 500, 50))])
 time_unit = datetime.timedelta(days=0.5)
 motion_models = [glimpse.CartesianMotionModel(
     xyi, time_unit=time_unit, dem=dem, dem_sigma=3, n=5000, xy_sigma=(2, 2),
@@ -71,10 +62,10 @@ motion_models = [glimpse.CartesianMotionModel(
 #     vrthz_sigma=(np.sqrt(50), np.pi, 0.2), arthz_sigma=(np.sqrt(8), 0.05, 0.2))
 #     for xyi in xy]
 tracker = glimpse.Tracker(observers=observers, viewshed=viewshed)
+print("\n****Tracking Now*****\n")
 tracks = tracker.track(motion_models=motion_models, tile_size=(15, 15),
-    parallel=32)
+    parallel=2**5)
 
 # ---- Plot tracks ----
 tracks.plot_xy(start=dict(color='green'), mean=dict(color='red'), sigma=dict(alpha=0.25))
 plt.show()
-
